6ab03e57bd71c46f4bb4fd27ea76de33b8ae48c8
Anton Khaynovskiy <a.khaynovskiy@skyeng.ru>
2023-04-10 12:44:11 +0200

    move to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'mocked-list';
-}
+export class AppComponent {}
